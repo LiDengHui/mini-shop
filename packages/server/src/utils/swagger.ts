46c02a7aa697eaeda77c3bc3fcc9a80e1dc36bd7
denghui.li <3379489032@qq.com>
2021-01-11 22:33:24 +0800

    ci: code review tools

--- packages/server/src/utils/swagger.ts
+++ packages/server/src/utils/swagger.ts
@@ -1,4 +1,4 @@
-export const resListSchema = (model: string) => {
+export const resSchema = (data: object) => {
     return {
         description: "ok",
         schema: {
@@ -12,40 +12,28 @@ export const resListSchema = (model: string) => {
                     type: "string",
                     default: "",
                 },
-                data: {
-                    type: "object",
-                    properties: {
-                        [model]: {
-                            type: "array",
-                            items: {
-                                $ref: `#/definitions/${model}`,
-                            },
-                        },
-                    },
-                },
+                data,
             },
         },
     };
 };
 
-export const resDetailSchema = (model: string) => {
-    return {
-        description: "ok",
-        schema: {
-            type: "object",
-            properties: {
-                code: {
-                    type: "number",
-                    default: 200,
-                },
-                message: {
-                    type: "string",
-                    default: "",
-                },
-                data: {
+export const resListSchema = (model: string) => {
+    return resSchema({
+        type: "object",
+        properties: {
+            [model]: {
+                type: "array",
+                items: {
                     $ref: `#/definitions/${model}`,
                 },
             },
         },
-    };
+    });
+};
+
+export const resDetailSchema = (model: string) => {
+    return resSchema({
+        $ref: `#/definitions/${model}`,
+    });
 };
